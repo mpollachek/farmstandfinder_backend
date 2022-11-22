601ba595e7b9edc66a5151a052d247006ed67b81
mpollachek <mpollachek81@gmail.com>
2022-11-22 18:03:10 -0500

    first commit - GeoJSON farm schema

---
+++ models/farmSchema.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+  rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: true
+  },
+  text: {
+      type: String,
+      required: true
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
+}, {
+  timestamps: true
+});
+
+const farmSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  location: {
+    type: {
+      type: String, // Don't do `{ location: { type: String } }`
+      default: 'Point', // 'location.type' must be 'Point'
+      required: true
+    },
+    coordinates: {
+      type: [Number],
+      required: true
+    }
+  },
+  address: {
+    road: String,
+    town: String,
+    state: String,
+    country: String
+  },
+  description: String,
+  products: {
+    type: [String]
+  },
+  seasons: {
+    type: [String],
+    enum: ['summer', 'spring', 'winter', 'fall']
+  },
+  image: {
+    type: [String]
+  },
+  featured: Boolean,
+  comments: [commentSchema],
+}, {
+  timestamps: true
+});
+
+const Farm = mongoose.model('Farm', farmSchema);
+
+module.exports = Farm;
