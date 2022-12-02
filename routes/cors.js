4240151b9ff377415e8112770d4d2ed0db4d60a8
mpollachek <mpollachek81@gmail.com>
2022-12-02 16:15:54 -0500

    whitelist ip

--- routes/cors.js
+++ routes/cors.js
@@ -1,6 +1,6 @@
 const cors = require('cors');
 
-const whitelist = ['http://localhost:3000', 'https://localhost:3443'];
+const whitelist = ['http://localhost:3000', 'http://192.168.56.1'];
 const corsOptionsDelegate = (req, callback) => {
     let corsOptions;
     console.log(req.header('Origin'));
